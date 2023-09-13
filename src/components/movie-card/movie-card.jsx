6e904e413d5d38a5959f40deae8b694e28582339
Ramiz Hida <ramizhida00@gmail.com>
2023-09-13 15:18:05 +0200

    Get Data From movie API, add prop constraints to components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MovieCard = ({ movie, onMovieClick }) => {
   return <div onClick={() => onMovieClick()}>{movie.title}</div>;
 };
 
 export default MovieCard;
+
+// Prop Constraints
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    genre: PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string,
+      bio: PropTypes.string,
+      birthday: PropTypes.string,
+    }),
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    image: PropTypes.string,
+    featured: PropTypes.bool,
+    id: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
