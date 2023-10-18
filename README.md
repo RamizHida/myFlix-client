583d2da6c057ec4d7060ed8ec8c3dd756a034113
Ramiz Hida <ramizhida00@gmail.com>
2023-10-18 02:47:24 +0200

    Implement Filter Feature

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Project testing with Parcel
+**_Overview_**
+
+myFlix-client is an app which allows it's users to view different movies and bookmark them for easy retrieval, via connecting to a movie database.
+
+**_Tools Used_**
+
+MERN (MongoDB, Express, React, and Node.js)
+
+**_Features_**
+
+- Ability to search for specific movie
+- Allows users to add movies and delete movies from their personal movie list
+- Allows for updating account info and delete account
---
+++ src/components/search-movie-form/search-movie-form.jsx
@@ -0,0 +1,42 @@
+import { filter } from 'lodash';
+import React from 'react';
+import { useState } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+
+const SearchMovieForm = ({ movies, setMovies }) => {
+  const [specificMovie, setSpecificMovie] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (specificMovie.length === 0) {
+      return alert('Please type a movie title');
+    }
+    console.log(specificMovie, 'specificMovie');
+    filterMovie = movies.filter((movie) => movie.title === specificMovie);
+    if (filterMovie.length === 0) {
+      return alert('No Such Movie Availble');
+    }
+    setMovies(filterMovie);
+    console.log(filterMovie, 'Filtered Movie');
+    localStorage.setItem('filteredMovie', JSON.stringify(filterMovie));
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit} className="formSearch">
+        <FormControl
+          placeholder="Search for movie by title"
+          type="text"
+          value={specificMovie}
+          onChange={(e) => setSpecificMovie(e.target.value)}
+          className="searchMovie"
+        />
+        <Button type="submit" className="btnSearch">
+          Search
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default SearchMovieForm;
